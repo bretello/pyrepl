412e814faa22cba3a19ff6b08b40373caf00f0c1
bretello <bretello@distruzione.org>
2024-04-29 13:14:27 +0200

    tests: add annotations to TestConsole/read_spec

--- tests/infrastructure.py
+++ tests/infrastructure.py
@@ -16,9 +16,9 @@
 # RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+from __future__ import annotations
 
-
-from typing import Tuple
+from typing import List, Optional, Tuple, Union
 
 from pyrepl.console import Console, Event
 from pyrepl.reader import Reader
@@ -32,38 +32,52 @@ class EqualsAnything:
 EA = EqualsAnything()
 
 
+Command = Union[
+    Tuple[str, Optional[str]],
+    Union[Tuple[str], str],
+]
+ExpectedScreen = Optional[List[str]]
+TestSpec = List[Tuple[Command, ExpectedScreen]]
+
+
 class TestConsole(Console):
-    def __init__(self, events, verbose=False):
+    def __init__(
+        self,
+        events: list[tuple[Command, ExpectedScreen]],
+        verbose: bool = False,
+    ):
         super().__init__(width=80, height=24, encoding="utf-8")
         self.events = events
-        self.next_screen = None
+        self.next_screen: list[str] | None = None
         self.verbose = verbose
 
-    def refresh(self, screen, xy: Tuple[int, int]):
+    def refresh(self, screen, xy: tuple[int, int]):
         if self.next_screen is not None:
             assert (
                 screen == self.next_screen
             ), f"[ {screen} != {self.next_screen} after {self.last_event_name} ]"
 
-    def get_event(self, block=True):
-        ev, sc = self.events.pop(0)
-        self.next_screen = sc
-        if not isinstance(ev, tuple):
-            ev = (ev, None)
-        self.last_event_name = ev[0]
+    def get_event(self, block: bool = True) -> Event:
+        event: Command
+        screen: ExpectedScreen
+        event, screen = self.events.pop(0)
+        self.next_screen = screen
+        if not isinstance(event, tuple):
+            event = (event, None)
+        self.last_event_name = event[0]
         if self.verbose:
-            print("event", ev)
-        return Event(*ev)
+            print(f"{event=}")
+        return Event(*event)
 
-    def getpending(self):
+    def getpending(self) -> Event:
         """Nothing pending, but do not return None here."""
-        return Event("key", "", b"")
+        return Event("key", "", "")
 
 
 class TestReader(Reader):
     __test__ = False
 
-    def get_prompt(self, lineno, cursor_on_line):
+    def get_prompt(self, lineno: int, cursor_on_line) -> str:
         return ""
 
     def refresh(self):
@@ -71,8 +85,9 @@ class TestReader(Reader):
         self.dirty = True
 
 
-def read_spec(test_spec, reader_class=TestReader):
+def read_spec(test_spec: TestSpec, reader_class=TestReader):
     # remember to finish your test_spec with 'accept' or similar!
     con = TestConsole(test_spec, verbose=True)
+
     reader = reader_class(con)
     reader.readline()
